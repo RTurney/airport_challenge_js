9c4f075cdfefb6b6951454e930ced0c05540879a
RTurney <r.turney93@gmail.com>
2021-02-13 18:10:16 +0000

    Complete takeOff plane so that it actually works. All tests green

--- src/airport.js
+++ src/airport.js
@@ -13,6 +13,8 @@ class Airport {
     this.hangar = this.hangar.filter((planeInHangar) => {
       if(planeInHangar.name === plane.name){
         return false;
+      } else {
+        return true;
       }
     });
   }
