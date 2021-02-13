5f695f2675a12fad164ee1486f74d0c0ec1f056e
RTurney <r.turney93@gmail.com>
2021-02-13 12:12:05 +0000

    Add hangar spec. 1 test red

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -7,4 +7,12 @@ describe('Airport', () => {
   it('is an Airport', () => {
     expect(heathrow).toBeInstanceOf(Airport);
   });
+
+  it('has an empty hangar',() => {
+    expect(heathrow.hangar).toEqual([]);
+  });
+
+  describe('.land', () => {
+
+  });
 });
