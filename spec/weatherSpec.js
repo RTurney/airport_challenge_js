e3c3db9ce043c20b5d8063b6e8f2297a28864b7a
RTurney <r.turney93@gmail.com>
2021-02-14 14:03:19 +0000

    Add spec for isStormy. All tests green

--- spec/weatherSpec.js
+++ spec/weatherSpec.js
@@ -10,5 +10,22 @@ describe('Weather', () => {
     it('is a class', () => {
       expect(weather).toBeInstanceOf(Weather);
     });
+
+    it('has a wind factor property', () => {
+      expect(weather.wind).toBeInstanceOf(Number);
+    });
+  });
+
+  describe('.isStormy', () => {
+
+    it('will return true if wind is over 80', () => {
+      spyOn(weather, "isStormy").and.returnValue(true);
+      expect(weather.isStormy()).toBe(true);
+    });
+
+    it('will return false if wind is below 80', () => {
+      spyOn(weather, "isStormy").and.returnValue(false);
+      expect(weather.isStormy()).toBe(false);
+    });
   });
 });
--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,11 @@
 // weather class
 class Weather {
-  
+
+  constructor() {
+    this.wind = Math.floor((Math.random() * 100) + 1);
+  }
+
+  isStormy() {
+    return this.wind >= 80 ? true : false;
+  }
 }
